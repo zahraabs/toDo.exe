05228d81721a6c66ea0f7900bc05115b1a669a29
zahraabs <zahraabbasiun@gmail.com>
2023-07-13 12:52:11 +0330

    new commit added

--- js/app.js
+++ js/app.js
@@ -3,24 +3,30 @@ var submit = document.querySelector(".form button");
 var inputTask = document.querySelector(".form input");
 var list = document.querySelector(".toDo .list");
 var closeIcons = document.querySelectorAll(".toDo i");
+var completedTasksHolder = document.querySelector(".done .list");
 
 showAllSavedItem();
 // add event
 list.addEventListener("click", icons)
+completedTasksHolder.addEventListener("click", icons)
 document.forms[0].addEventListener("submit", addItem);
 addEventListener("click" , saveEditedItem);
 
 // add item
 function addItem(event) {
     event.preventDefault();
-    var text = inputTask.value;
+    var text = inputTask.value.trim();
     
+    if (text === "") {
+        alert('Please enter a task!');
+    }
     // create new items in list
-    if (text != "") {
+    else if (text != "") {
         addToStorage(text);
         showAllSavedItem();
+       
     }
-
+    bindTaskEvents(text, taskCompleted);
     inputTask.value = '';
     inputTask.focus();
 }
@@ -99,7 +105,6 @@ function editItem(element) {
     newInput.setAttribute("type", "text");
     newInput.value = element.parentElement.parentElement.children[0].children[1].innerText;
     
-    // console.log(element.parentElement.parentElement.children[0].children[1]);
     element.parentElement.parentElement.children[0].children[1].remove();
     element.parentElement.parentElement.children[0].insertBefore(newInput, element.parentElement.parentElement.children[0].lastchild)
     newInput.focus();
@@ -110,7 +115,7 @@ function saveEditedItem(){
     var old = oldItems();
 
     for (var i = 0; i < old.length; i++) {
-        if (old[i].id == editing.parentElement.parentElement.dataset.id) {
+        if (old[i].id == editing.parentElement.dataset.id) {
             old[i].content = editing.value;
         }
 
@@ -122,9 +127,61 @@ function saveEditedItem(){
 function closeEditingInput(element){
     var newLabel = document.createElement("label");
     var newText = document.createTextNode( element.parentElement.parentElement.children[0].children[1].value);
-    newLabel.htmlFor = 'item';
+    newLabel.htmlFor = element.parentElement.parentElement.children[0].children[0].id;
     newLabel.appendChild(newText);
 
     element.parentElement.parentElement.children[0].insertBefore(newLabel ,element.parentElement.parentElement.children[0].lastchild)
     element.remove();
 }
+
+//Mark a task as complete
+function taskCompleted () {
+    console.log("Task Complete...");
+   //When the Checkbox is checked 
+   //Append the task list item to the #done ul
+    var listItem = this.parentNode.parentNode;
+    completedTasksHolder.appendChild(listItem);
+    bindTaskEvents(listItem, taskIncomplete);
+ }
+ 
+ 
+//  //Mark a task as incomplete
+ function taskIncomplete() {
+      console.log("Task Incomplete...");
+      //When the checkbox is unchecked appendTo toDo
+   var listItem = this.parentNode.parentNode;
+   list.appendChild(listItem);
+   bindTaskEvents(listItem, taskCompleted);
+ }
+ 
+ 
+//  //Set the click handler to the addTask function
+ submit.addEventListener("click", addItem); 
+ 
+ 
+ function bindTaskEvents(taskListItem, checkBoxEventHandler) {
+       console.log("Bind List item events");
+       // select listitems chidlren
+       var checkBox = taskListItem.querySelector('input[type="checkbox"]');
+     var editIcon = taskListItem.querySelector("i.bx-edit");
+     var deleteIcon = taskListItem.querySelector("i.bx-x");
+         //bind editTask to edit Icon
+       editIcon.onclick = editItem;
+         //bind deleteTask to delete Icon
+          deleteIcon.onclick = deleteItem;
+         //bind checkBoxEventHandler to checkbox
+       checkBox.onchange = checkBoxEventHandler;
+  
+ }
+ 
+ //cycle over incompleteTaskHolder ul list items
+ for (var i = 0; i < list.children.length; i ++) {
+   //bind events to list item's children (taskCompleted)	
+   bindTaskEvents(list.children[i], taskCompleted);
+ }
+ 
+ //cycle over completedTaskHolder ul list items
+ for (var i = 0; i < completedTasksHolder.children.length; i ++) {
+   //bind events to list item's children (taskCompleted)	
+   bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
+ }
