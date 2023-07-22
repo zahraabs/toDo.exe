7ad205d18fc6b12915a16b0eb90363dc44ee202c
zahraabs <zahraabbasiun@gmail.com>
2023-07-22 10:11:38 +0330

    last commit added

--- js/app.js
+++ js/app.js
@@ -3,32 +3,31 @@ var submit = document.querySelector(".form button");
 var inputTask = document.querySelector(".form input");
 var list = document.querySelector(".toDo .list");
 var closeIcons = document.querySelectorAll(".toDo i");
-var completedTasksHolder = document.querySelector(".done .list");
+var completedTask = document.querySelector(".done .list");
 
-showAllSavedItem();
-// add event
-list.addEventListener("click", icons)
-completedTasksHolder.addEventListener("click", icons)
+showUnComplete();
+showCompleteList();
+// events
+list.addEventListener("click", icons);
+completedTask.addEventListener("click" , icons);
+
+
 document.forms[0].addEventListener("submit", addItem);
 addEventListener("click", saveEditedItem);
 
 // add item
 function addItem(event) {
     event.preventDefault();
-    var text = inputTask.value.trim();
-
+    var text = inputTask.value;
     if (text === "") {
         alert('Please enter a task!');
     }
-    // create new items in list
     else if (text != "") {
         addToStorage(text);
-        showAllSavedItem();
-
+        showUnComplete();
     }
     inputTask.value = '';
     inputTask.focus();
-    bindTaskEvents(list, taskCompleted);
 }
 
 //function for list of icons
@@ -39,32 +38,23 @@ function icons(e) {
         e.target.parentElement.parentElement.remove();
     } else if (e.target.classList.contains("bx-edit")) {
         editItem(e.target);
+    } else if (e.target.nodeName === "INPUT") {
+        complete(e.target);
     }
-
 }
 
 // add old items to local storage
 function addToStorage(text) {
     var currentNames = oldItems();
-    // var completedNames = oldItems();
     var date = new Date();
     var newItem = {
         id: date.getTime(),
         content: text,
-        isComplete: false
+        isDone: false
     }
 
-    // newItem.isComplete = !newItem.isComplete;
-    
-    //  var completeItem = {
-    //     id: date.getTime(),
-    //     content: key,
-    // }
-
     currentNames.push(newItem);
-    // completedNames.push(completeItem);
-    localStorage.setItem("names", JSON.stringify(currentNames));
-    // localStorage.setItem("names", JSON.stringify(completedNames));
+    localStorage.setItem("names", JSON.stringify(currentNames))
 }
 
 // get old names and return an array
@@ -78,24 +68,26 @@ function oldItems() {
     return oldNames;
 }
 
-// function for showing all saved old items in storage
-function showAllSavedItem() {
+// function for showing all uncomplete items in storage
+function showUnComplete() {
     var old = oldItems();
     list.innerHTML = "";
     for (var i = 0; i < old.length; i++) {
-
-        list.innerHTML += `<li data-id="${old[i].id}">
-        <div>
-        <input type="checkbox" name="item" id="item${i}" /><label for="item${i}"
-        >${old[i].content}</label
-        >
-        </div>
-        <div class="icons">
-        <i class='bx bx-edit'></i>
-        <i class="bx bx-x"></i>
-                              </div>
-                              
+        if (old[i].isDone == false) {
+            list.innerHTML += `<li data-id="${old[i].id}">
+                                    <div>
+                                    <input type="checkbox" name="item" id="item${i}" />
+                                    <label for="item${i}"
+                                    >${old[i].content}
+                                    </label>
+                                    </div>
+                                    <div class="icons">
+                                    <i class='bx bx-edit'></i>
+                                    <i class="bx bx-x"></i>
+                                    </div>
                               </li>`;
+        }
+
     }
 }
 
@@ -106,6 +98,7 @@ function deleteItem(id) {
         if (old[i].id == id) {
             old.splice(i, 1);
         }
+
     }
     localStorage.setItem("names", JSON.stringify(old))
 }
@@ -114,25 +107,38 @@ function editItem(element) {
     var newInput = document.createElement("input");
     newInput.setAttribute("type", "text");
     newInput.value = element.parentElement.parentElement.children[0].children[1].innerText;
-
     element.parentElement.parentElement.children[0].children[1].remove();
     element.parentElement.parentElement.children[0].insertBefore(newInput, element.parentElement.parentElement.children[0].lastchild)
     newInput.focus();
 }
 
+function complete(element) {
+    var old = oldItems();
+    for (var i = 0; i < old.length; i++) {
+        if (old[i].id == element.parentElement.parentElement.dataset.id) {
+            old[i].isDone = !old[i].isDone;
+        }
+    }
+    localStorage.setItem("names", JSON.stringify(old));
+
+    showCompleteList();
+    showUnComplete();
+}
+
 function saveEditedItem() {
     var editing = list.querySelector("input[type='text']");
     var old = oldItems();
-
-    for (var i = 0; i < old.length; i++) {
-        if (old[i].id == editing.parentElement.parentElement.dataset.id) {
-            old[i].content = editing.value;
+    if (editing == null) {
+        return [];
+    } else {
+        for (var i = 0; i < old.length; i++) {
+            if (old[i].id == editing.parentElement.parentElement.dataset.id) {
+                old[i].content = editing.value;
+            }
         }
-
+        localStorage.setItem("names", JSON.stringify(old));
+        closeEditingInput(editing);
     }
-    isDone = true;
-    localStorage.setItem("names", JSON.stringify(old))
-    closeEditingInput(editing);
 }
 
 function closeEditingInput(element) {
@@ -145,73 +151,29 @@ function closeEditingInput(element) {
     element.remove();
 }
 
-//Mark a task as complete
-function taskCompleted() {
-    //When the Checkbox is checked 
-    //Append the task list item to the #done ul
-    var listItem = this.parentNode.parentNode;
-    completedTasksHolder.appendChild(listItem);
-    bindTaskEvents(listItem, taskIncomplete);
-    // var oldNames;
-    // oldNames = JSON.parse(localStorage.getItem("names"));
-    // oldNames[0].isComplete = !oldNames[0].isComplete;
-    // for (var i = 0; i < oldNames.length; i++) {
-    //     if (oldNames[0].isComplete == true) {
-    //         oldNames.splice(i, 1);
-    //     }
-    //     localStorage.setItem("names", JSON.stringify(oldNames));
-       
-    // }
-   
-    
-    // ***********************************************************
-    // listItem.children[1].children[0].remove();
-}
+function showCompleteList() {
+    var old = oldItems();
+    completedTask.innerHTML = "";
+    for (var i = 0; i < old.length; i++) {
+        if (old[i].isDone == true) {
 
+            completedTask.innerHTML += `<li data-id="${old[i].id}">
+                                            <div>
+                                            <input type="checkbox" name="item" id="item${i}" checked/>
+                                            <label for="item${i}"
+                                                >${old[i].content}
+                                                </label>
+                                            </div>
+                                            <div class="icons">
+                                            <i class="bx bx-x"></i>
+                                            </div>
+        
+                                      </li>`;
 
-//  //Mark a task as incomplete
-function taskIncomplete() {
-    //When the checkbox is unchecked appendTo toDo
-
-    //   ***********************************************
-    // var newEditIcon = document.querySelector("i");
-    // newEditIcon.className = "bx bx-edit";
-
-
-
-    var listItem = this.parentNode.parentNode;
-    list.appendChild(listItem);
-    bindTaskEvents(listItem, taskCompleted);
-}
-
-
-//  //Set the click handler to the addTask function
-submit.addEventListener("click", addItem);
-
-
-function bindTaskEvents(taskListItem, checkBoxEventHandler) {
-    // select listitems chidlren
-    var checkBox = taskListItem.querySelector('input[type="checkbox"]');
-    var editIcon = taskListItem.querySelector("i.bx-edit");
-
-    var deleteIcon = taskListItem.querySelector("i.bx-x");
-    //bind editTask to edit Icon
-    editIcon.onclick = editItem;
-    //bind deleteTask to delete Icon
-    deleteIcon.onclick = deleteItem;
-    //bind checkBoxEventHandler to checkbox
-    checkBox.onchange = checkBoxEventHandler;
+        }
+    }
 
 }
 
-//cycle over incompleteTaskHolder ul list items
-for (var i = 0; i < list.children.length; i++) {
-    //bind events to list item's children (taskCompleted)	
-    bindTaskEvents(list.children[i], taskCompleted);
-}
 
-//cycle over completedTaskHolder ul list items
-for (var i = 0; i < completedTasksHolder.children.length; i++) {
-    //bind events to list item's children (taskCompleted)	
-    bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
-}
+
